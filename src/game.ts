4b41d12a3ff8baa400a1f8288005c7229d4e01d5
Renato Soares <renatosoarespro@hotmail.com>
2020-08-17 18:23:52 -0300

    make something happen

--- src/game.ts
+++ src/game.ts
@@ -1,10 +1,18 @@
 import 'phaser';
+import { GameScene } from './gameScene';
 
 const config: Phaser.Types.Core.GameConfig = {
     width: 390,
     height: 600,
     parent: 'game',
-    backgroundColor: "#29217E"
+    scene: [GameScene],
+    physics: {
+        default: 'arcade',
+        arcade: {
+            debug: false,
+        },
+    },
+    backgroundColor: '#29217E',
 };
 
 export class Game extends Phaser.Game {
